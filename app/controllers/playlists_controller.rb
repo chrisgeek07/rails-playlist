c2e2e77045221a01dc1bba3f6a23b8297730d63a
Christian BeauxYeux <gildweek@gmail.com>
2021-11-05 11:39:35 +0400

    Added controller methods and respective views

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,5 +2,18 @@ class PlaylistsController < ApplicationController
   def index
     @playlists = Playlist.all
   end
-  
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :genre, :description, :photo)
+  end
 end
